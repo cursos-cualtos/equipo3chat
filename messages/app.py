e3feaf891e7ca13176f336135d3e2dab321f27ca
Héctor <hdeandaf@hotmail.com>
2020-07-14 17:28:28 -0500

    def messages_add

--- messages/app.py
+++ messages/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from markupsafe import escape
 from data import messages
+from db_utils import add_message
 import json
 
 app = Flask(__name__)
@@ -14,6 +15,10 @@ def greeting(message_id):
     message = {'message': 'Hello user'}
     print(message_id)
     return json.dumps(messages[message_id])
+
+@app.route('/messages/add')
+def messages_add():
+    add_message()
 
 if __name__ == "__main__":
     app.run(port=5002)

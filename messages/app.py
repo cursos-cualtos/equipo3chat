bcac5d85348d1d04539ae95a0d269731140200ff
Emmanuel Rubio <emmanuelr@bluetrailsoft.com>
2020-07-14 18:00:23 -0500

    create and get messages in postman

--- messages/app.py
+++ messages/app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from markupsafe import escape
+from flask import request
 from data import messages
 from db_utils import add_message
 import json
+import db_utils
 
 app = Flask(__name__)
 
 @app.route('/messages')
 def index():
-    return json.dumps(messages)
+    # result = 
+    return json.dumps(db_utils.get_all_messages(), default=str)
 
 @app.route('/messages/<int:message_id>', methods=["GET"])
 def greeting(message_id):
@@ -16,9 +19,11 @@ def greeting(message_id):
     print(message_id)
     return json.dumps(messages[message_id])
 
-@app.route('/messages/add')
+@app.route('/messages/add', methods=["POST"])
 def messages_add():
-    add_message()
+    print(request.json)
+    add_message(request.json)
+    return json.dumps({})
 
 if __name__ == "__main__":
     app.run(port=5002)
